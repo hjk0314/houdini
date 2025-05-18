b1de534d04a96198aea073cc4b3589355cfd14ba
hjk0314 <hjk0314@gmail.com>
2025-05-18 22:43:45 +0900

    Two Example Python to Houdini scripts

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
+
+
+""" Create geo Node """
 # import hou
-
-
 obj = hou.node("/obj")
 if obj is not None:
     geo_node = obj.createNode("geo", "geo1")
     geo_node.moveToGoodPosition()
 
+
+""" Change a specific parts to a primitive name. """
+# node = hou.pwd()
+# geo = node.geometry()
+path_attr = geo.findPrimAttrib("path")
+if path_attr is None:
+    raise hou.NodeError("No 'path' attribute found")
+for prim in geo.prims():
+    p = prim.stringAttribValue("path")
+    parts = p.split("/")
+    prim.setAttribValue(path_attr, parts[-2])
